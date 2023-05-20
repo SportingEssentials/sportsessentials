6b1e05463d79dcd5ef0c9368431e72ff82e02446
SportingEssentials <sportingessentials11@gmail.com>
2023-05-20 17:27:10 -0400

    debug=true

--- sportsessentials/settings.py
+++ sportsessentials/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eq7=g=37ktpvyzrr75n*7aud7$d4$(+!ncr5$6b5#-po+8zmrc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
