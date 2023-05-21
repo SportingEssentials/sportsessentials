ad042db0fcad759600825cf56d85935228b5a1bd
SportingEssentials <sportingessentials11@gmail.com>
2023-05-21 19:46:09 -0400

    newdatabase

--- sportsessentials/settings.py
+++ sportsessentials/settings.py
@@ -85,6 +85,10 @@ WSGI_APPLICATION = 'sportsessentials.wsgi.application'
 #}
 
 DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
