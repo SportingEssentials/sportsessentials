2a135bf5285b0903764856357a950d210f0f13fe
SportingEssentials <sportingessentials11@gmail.com>
2023-05-24 22:53:37 -0400

    effe

--- sportsessentials/wsgi.py
+++ sportsessentials/wsgi.py
@@ -2,13 +2,12 @@ import os
 import sys
 
 # Add your project directory to the sys.path
-path = '/home/Hoopessentials/sportsessentials '
+path = '/home/Hoopessentials/sportsessentials'
 if path not in sys.path:
     sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportsessentials.settings')
-
 
 application = get_wsgi_application()
